995a5dfb764a902e03b687d0247fe3a2f0dc5f93
Gabriel Silva Santos <91688865+gabrielsantos7@users.noreply.github.com>
2023-09-28 09:14:01 -0300

    integrate service

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlunoRoutingModule } from './aluno-routing.module';
+import { AlunoService } from './aluno.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     AlunoRoutingModule
-  ]
+  ],
+  exports: [AlunoService],
 })
 export class AlunoModule { }
