d81db48efee2fc08644a4244f14a7c750a6efd1e
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-09-30 18:49:55 -0300

    configure base routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './home/index/index.component';
+// import { IndexComponent } from './home/index/index.component';
 
 const routes: Routes = [
-  
+
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ShowDetailsComponent } from '../aluno/show-details/show-details.component';
+import { ListAllComponent } from '../aluno/list-all/list-all.component';
 
 const routes: Routes = [
-  { path: 'alunos', component: IndexComponent },
+  { path: 'alunos', component: ListAllComponent },
   { path: 'alunos/:id', component: ShowDetailsComponent },
   { path: '', component: IndexComponent }
 ];
