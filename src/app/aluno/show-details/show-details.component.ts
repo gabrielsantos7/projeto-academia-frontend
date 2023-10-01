9a53409ecf8000b6e5260d1d5c4518fb4458c0a7
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-10-01 09:30:21 -0300

    create alert message

--- src/app/aluno/show-details/show-details.component.ts
+++ src/app/aluno/show-details/show-details.component.ts
@@ -3,7 +3,7 @@ import Avaliacao from 'src/app/shared/models/Avaliacao';
 import Endereco from 'src/app/shared/models/Endereco';
 import { AlunoService } from '../aluno.service';
 import Aluno from 'src/app/shared/models/Aluno';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-show-details',
@@ -13,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ShowDetailsComponent implements OnInit {
   active: boolean = true;
   isLoading: boolean = true;
+  alunoId: number = 0;
   avaliacoes: Avaliacao[] = [];
   endereco: Endereco = {
     id: 0,
@@ -30,12 +31,13 @@ export class ShowDetailsComponent implements OnInit {
 
   constructor(
     private alunoService: AlunoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     const idAluno = Number(this.route.snapshot.paramMap.get('id'));
-
+    this.alunoId = idAluno;
     this.alunoService.getAlunoById(idAluno).subscribe({
       next: (aluno: Aluno) => {
         this.endereco = aluno.endereco;
@@ -47,5 +49,9 @@ export class ShowDetailsComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  backHome() {
+    this.router.navigateByUrl('/alunos');
   }
 }
